37f6968e1bc9fc80c37266105de22f5b7c974a7a
MaddHatt-PM <MaddHatt.pm@gmail.com>
2021-09-01 21:28:11 -0400

    reimplemented specific vocab word printing

--- GermanFlashcards.py
+++ GermanFlashcards.py
@@ -7,41 +7,21 @@ import time
 import json
 import random
 # import GuessTheTranslation
-import utilities
-
-# Starter method
-
-
-def ___init___():
-    utilities.clear_console()
-    displayDataLoadMenu()
-
+from utilities import *
 
 # ---------------------------------------------------------------------
-# --- Helper Methods ---
+# --- Starter Method ---
 # ----------------------
-def printFlashCard(flashcard:dict):
-    if (len(str(flashcard[key_gender])) != 0):
-        print(key_gender + ": " + flashcard[key_gender])
-    if (len(str(flashcard[key_wort_deutsch])) != 0):
-        print(key_wort_deutsch + ": " + flashcard[key_wort_deutsch])
-    if (len(str(flashcard[id_englisch])) != 0):
-        print(id_englisch + ": " + flashcard[id_englisch])
-    if (len(str(flashcard[id_kategorie])) != 0):
-        print(id_kategorie + ": " + flashcard[id_kategorie])
+
+def ___init___():
+    clear_console()
+    displayDataLoadMenu()
+
 
 
 # ---------------------------------------------------------------------
 # --- Main Methods ---
 # --------------------
-
-default_data_path = "Deutsch Vokabeln.json"
-# Identifier variables from my spreadsheet, custom ones aren't going to work
-key_gender = "Gender"
-key_wort_deutsch = "Wort Deutsch"
-id_englisch = "Englisch"
-id_kategorie = "Kategorie"
-
 
 # Get the vocabulary words from a spreadsheet thats been converted into a JSON file
 def displayDataLoadMenu():
@@ -53,24 +33,23 @@ def displayDataLoadMenu():
         print("Invalid selection was made... try again")
         displayDataLoadMenu()
 
-    filepath = default_data_path
+    filepath = "Deutsch Vokabeln.json"
 
     if(dataLoadID == 1):
         filepath = input("customDataPath: ")
 
-    vocabData = utilities.GermanData()
-    vocabData.load_vocabulary_dict(utilities.load_json_file(filepath))
-    print(vocabData.get_random_flashcard())
+    germanData = GermanData()
+    germanData.load_vocabulary_dict(load_json_file(filepath))
 
-    displayMainMenu()
+    displayMainMenu(germanData)
 
-def displayMainMenu():
+def displayMainMenu(germanData:GermanData):
     while True:
-        utilities.print_seperator()
+        print_seperator()
         print("[0] Play simple flashcards")
         print("[1] Play pick the correct one out of three")
-        print("[8] Output specific card by index")
-        print("[9] Output entire vocab file via ID")
+        print("[9] Output specific card by index")
+
         try:
             selectionID = input("Select [#] to execute: ")
         except:
@@ -79,41 +58,36 @@ def displayMainMenu():
         if(selectionID == "0"):
             print("Reworking")
         elif(selectionID == "1"):
-            playFindTheCorrectOne()
-        elif(selectionID == "8"):
-            printIndividualVocabCard()
+            playFindTheCorrectOne(germanData)
         elif(selectionID == "9"):
-            printEntireVocabFile()
+            print_specific_vocab_word(germanData)
         elif(selectionID == "~"):
             print("You're already on the main menu")
-            utilities.print_seperator()
+            print_seperator(germanData)
         else:
             print("Invalid selection was made... try again")
 
 
 
-def printIndividualVocabCard(vocabWords):
-    vocabLength = (len(dict(vocabWords)))
+def print_specific_vocab_word(germanData:GermanData):
+    count = len(germanData.vocab_words)
+
     while True:
         try:
-            utilities.print_seperator()
-            choice = input(
-                "Enter '~' to return or a value between 0-" + str(vocabLength) + ": ")
+            print_seperator()
+            choice = input("Enter '~' to return or a value between 0-" + str(count) + ": ")
             # Return to main menu
             if (choice == "~"):
                 return
 
-            id = int(choice)
-            printFlashCard(vocabWords[str(id)])
+            germanData.get_flashcard(int(choice)).print_card()
+            
         except:
             print("Invalid data entry")
 
 def playFindTheCorrectOne(vocabWords):
     print("TODO")
-    utilities.print_seperator()
-
-def printEntireVocabFile(vocabWords):
-    print("TODO")
+    print_seperator()
 
 # ---------------------------------------------------------------------
 
