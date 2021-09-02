9425386564c31cd796f1504929f33507d2a38938
MaddHatt-PM <MaddHatt.pm@gmail.com>
2021-09-01 22:23:35 -0400

    Reimplemented game but without modifiers

--- GuessTheTranslation.py
+++ GuessTheTranslation.py
@@ -4,8 +4,13 @@ key__flip_rate = "flip_rate"
 key__kategorie = "kategorie"
 all_categories = "_all"
 
+def config_options(game_options:dict):
+    return game_options
+
 def play_game(germanData:GermanData):
     game_options = { key__flip_rate: 0.5, key__kategorie:all_categories  }
+    correct_count = 0
+    card_count = 0
     
     print_seperator()
     print("Welcome to Guess the Translation!")
@@ -15,15 +20,37 @@ def play_game(germanData:GermanData):
     while True:
         print_seperator()
 
+        # Chosee card and setup variables
+        chosenCard = germanData.get_random_flashcard(game_options.get(key__kategorie))
+        question = chosenCard.gender + chosenCard.deutsch
+        answer =  chosenCard.englisch
+
+        flipLanguage = random.random() < game_options.get(key__flip_rate)
+        answer_language = "English "
+        if (flipLanguage):
+            question, answer = answer, question
+            answer_language = "Deutsch "
+
+        print("Whats the " + answer_language + "translation of: " + question)
         choice = input("Enter answer: ")
 
         if (choice == "/exit"):
             return
 
         if (choice == "/options"):
-            config_options(game_options)
+            game_options = config_options(game_options)
+            continue
 
-    return
+        if (choice.lower() == answer.lower):
+            print("Correct")
+        else:
+            print("Incorrect, the correct answer was: " + answer)
+            if(input("Mark as correct anyways? Y or type the answer to advance: ") == 'Y'):
+                correct_count += 1
+        
+        card_count += 1
+        print("Card's played: ", str(card_count)," Correct Responses: ", str(correct_count))
 
-def config_options(game_options:dict):
-    return
+
+    
+
